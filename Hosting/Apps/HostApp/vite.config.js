5730ebeac394d8dcb11b23d08109f10b90857223
shamon2654 <shamons226@gmail.com>
2024-09-06 10:00:27 +0530

    feat:port set in the main app

--- Hosting/Apps/HostApp/vite.config.js
+++ Hosting/Apps/HostApp/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5170,  // Set a unique port for app1
+  },
+});
